4e5689231f27f9276916db7fb8678886b0bd43c4
nihaprezz <sergio13prez@gmail.com>
2019-11-13 09:38:38 -0500

    fixed multiple images being added

--- src/backupGameLogic.js
+++ src/backupGameLogic.js
@@ -11,18 +11,23 @@ function addToPlate(event){
             //created the image tag that will be inside of the show plate
             
             let plateContainerDiv = document.querySelector(`#ingredient-${clickedIngredientId}`) //grabbing the right div depending on the ID
-        
-            plateContainerDiv.classList.add("ui", "small", "image")
-            plateContainerDiv.classList.add('plate-container-grid-items')
             
 
-            let plateImage = document.createElement('img')
-            plateImage.src = event.target.src
-            //resetting the data set attributes for the new image tag created 
-            plateImage.dataset.ingredientId = event.target.dataset.ingredientId
-            plateImage.dataset.ingredientName = event.target.dataset.ingredientName
+            //checks if the plate container has a image already in it.
+            if (plateContainerDiv.children.length === 0 ){
 
-            plateContainerDiv.append(plateImage)
+                plateContainerDiv.classList.add("ui", "small", "image")
+                plateContainerDiv.classList.add('plate-container-grid-items')
+                
+
+                let plateImage = document.createElement('img')
+                plateImage.src = event.target.src
+                //resetting the data set attributes for the new image tag created 
+                plateImage.dataset.ingredientId = event.target.dataset.ingredientId
+                plateImage.dataset.ingredientName = event.target.dataset.ingredientName
+
+                plateContainerDiv.append(plateImage)
+            }           
         }
     })
 
