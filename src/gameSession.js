5b309181b047bc4223857b96719a4956995be5b3
nihaprezz <sergio13prez@gmail.com>
2019-11-14 09:57:57 -0500

    finished beating game screen

--- src/gameSession.js
+++ src/gameSession.js
@@ -8,6 +8,8 @@ function nextLevel(){
     //clears the all-page screen
     let bodyTag = document.querySelector('#all-page')
     bodyTag.className = "";
+
+
     currentBurger++; //moves to the next burger
 
 
@@ -31,9 +33,51 @@ function nextLevel(){
             difficultyTimer = 12;
             break;  
     }
+
     gameSessionTimer = difficultyTimer;
     // difficultyTimer = difficultyTimer + 2; 
     //increments the timer
 
-    fetchFirstBurger()
+
+    //check and if the level is greater then 10, it will render the beating game screen
+    if (currentBurger < 10 ){
+        fetchFirstBurger() //starts the next session
+    } else {
+        renderBeatGameScreen()
+    }
+
+}
+
+function renderBeatGameScreen(){
+    let currentPage = document.querySelector('#all-page')
+    currentPage.classList.add('win-lose-screen');
+
+    let winLoseCard = document.createElement('div')
+    winLoseCard.classList.add('win-lose-card')
+    winLoseCard.classList.add('beat-game-card-styling');
+
+    let iconContainer = document.createElement('div')
+    iconContainer.classList.add('beat-game-style')
+
+    let gif = document.createElement('div');
+    gif.innerHTML = `
+<iframe src="https://giphy.com/embed/Qsb49tomju4vca9Sfu" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`
+
+    // let sadIcon = document.createElement('i')
+    // sadIcon.classList.add('far', 'fa-frown-open', 'fa-10x')
+
+    let lostText = document.createElement('h2')
+    lostText.classList.add('beat-game-card-text');
+    lostText.innerHTML = 'You have beaten all the levels. <br> You are the BURGER DINER MASTER!'
+
+    let tryAgainButton = document.createElement('button')
+    tryAgainButton.addEventListener('click', reloadPage)
+    
+    //ui inverted teal basic button
+    tryAgainButton.classList.add('ui', 'inverted', 'teal', 'basic', 'button')
+    tryAgainButton.innerText = "Home Screen"
+
+    iconContainer.append(gif)
+    winLoseCard.append(iconContainer, lostText, tryAgainButton)
+    currentPage.append(winLoseCard)
 }
